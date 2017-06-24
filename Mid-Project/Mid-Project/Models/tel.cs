fba87ff35e6845a7077f0a796f1e26cd99179e9d
Kariko <925433739@qq.com>
2017-06-24 10:22:33 +0800

    address_list

---
+++ Mid-Project/Mid-Project/Models/tel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace Mid_Project.Models
+{
+    class Tel
+    {
+        public Tel(string na, string nu,  string g, ImageSource s)
+        {
+            this.num = nu;
+            this.name = na;
+            this.group = g;
+            this.id = Guid.NewGuid().ToString();
+            //this.messages = t + "："+ d + "                     Due Date：" + date;
+            src = s;
+        }
+
+        private string id;
+        public string num { get; set; }
+        public string name { get; set; }
+        public string group { get; set; }
+       // public DateTime  { get; set; }
+        //public bool? completed { get; set; }
+
+        public ImageSource src;
+
+        //private string messages;
+        //public string Messages { get { return messages; } set { messages = title + ":" + details + "   "; } }
+    }
+}
---
+++ Mid-Project/Mid-Project/ViewModels/Telview.cs
@@ -0,0 +1,76 @@
+﻿using SQLitePCL;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace Mid_Project.ViewModels
+{
+    class Telview
+    {
+        private ObservableCollection<Models.Tel> allTels = new ObservableCollection<Models.Tel>();
+
+        public ObservableCollection<Models.Tel> Alltel { get { return this.allTels; } }
+
+        private Models.Tel selectTel = default(Models.Tel);
+
+        public Models.Tel SelectTel { get { return selectTel; } set { this.selectTel = value; } }
+
+        private Models.Tel updateTel = default(Models.Tel);
+        public Models.Tel UpdateTel { get { return updateTel; } set { this.updateTel = value; } }
+
+        public Telview(object sender)
+        {
+            Image img = sender as Image;
+
+            //this.allTels.Add(new Models.Tel("陈布丁", "18819253733", "智障队友", new BitmapImage(new Uri("ms-appx://Todos/Assets/p1.jpg"))));
+            //this.allTels.Add(new Models.Tel("陆大白", "15626477044", "智障队友", new BitmapImage(new Uri("ms-appx://Todos/Assets/p2.jpg"))));
+            //this.allTels.Add(new Models.Tel("圆滚滚", "18819253733", "智障队友", new BitmapImage(new Uri("ms-appx://Todos/Assets/p3.jpg"))));
+            //this.allTels.Add(new Models.Tel("不知名师姐", "15626477044", "智障队友", new BitmapImage(new Uri("ms-appx://Todos/Assets/p4.jpg"))));
+            int i;
+            string s1, s2, s3, s4;
+            for (i = 1; i <= 200; i++)
+            {
+                using (var statement = App.conn.Prepare("SELECT Name, Number, Group_By, head FROM Tels WHERE Id = ?"))
+                {
+                    statement.Bind(1, i);
+                    if (SQLiteResult.DONE != statement.Step())
+                    {
+                        s1 = (string)statement[0];
+                        s2 = (string)statement[1];
+                        s3 = (string)statement[2];
+                        s4 = (string)statement[3];
+                        this.allTels.Add(new Models.Tel(s1, s2, s3, new BitmapImage(new Uri(s4))));
+                    }
+                }
+            }
+
+        }
+
+        public void AddTel(string name, string num, string group, ImageSource src)
+        {
+            this.allTels.Add(new Models.Tel(name, num, group, src));
+        }
+
+
+        public void Remove()
+        {
+            this.allTels.Remove(selectTel);
+            this.selectTel = null;
+        }
+
+        public void GetUpdateItem()
+        {
+            if (this.allTels != null)
+            {
+                this.updateTel = this.allTels.Last();
+            }
+        }
+
+    }
+}
